a77e823ece2b20b3e6261560668a46642f5f4be3
PhoeKae <kaungkaung.pb8812@gmail.com>
2025-06-12 19:14:54 +0630

    new latest

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function viewCertificate() {
   window.open('photo/JAVA- B5-002 Kaung Zaw Thein.jpg', '_blank');
 }
+
+function viewCertificate2() {
+  window.open('photo/certificate.pdf', '_blank');
+}
